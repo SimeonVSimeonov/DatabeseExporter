038ca53aca8e0b658894a551a9f27ad737b132a8
ssimeonoff <simeon@rubisoft.net>
2020-01-24 11:51:16 +0200

    Established connection with PostgreSQL DB

---
+++ dbexport/dbexport/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace dbexport
+{
+    public static class Configuration
+    {
+        public const string NpgsqlConnectionString =
+            "Host=127.0.0.1;Username=postgres;Password=test;Database=university";
+
+        public const string MariaDbConnectionString = "";
+    }
+}
