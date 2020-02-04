beb6700d6e4d407ef0489d85c6f96f8c45700b3b
ssimeonoff <simeon@rubisoft.net>
2020-02-04 16:07:06 +0200

    Create Db entities

---
+++ dbexport/dbexport/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace dbexport.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public byte Age { get; set; }
+        public int TownId { get; set; }
+    }
+}
