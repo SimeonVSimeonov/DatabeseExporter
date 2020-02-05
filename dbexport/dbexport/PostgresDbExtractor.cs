b5d35f5345bd62e734ff2210da8f43c6af618468
ssimeonoff <simeon@rubisoft.net>
2020-02-05 15:35:20 +0200

    Change extractor to use more common sql query

--- dbexport/dbexport/PostgresDbExtractor.cs
+++ dbexport/dbexport/PostgresDbExtractor.cs
@@ -1,5 +1,8 @@
 ﻿using System.Data;
 using System.Data.Common;
+using System.IO;
+using System.Text;
+using Npgsql;
 
 namespace dbexport
 {
@@ -10,24 +13,45 @@ namespace dbexport
             if (connection == null)
                 return;
 
+            StringBuilder stringBuilder = new StringBuilder();
+
             using (connection)
             {
                 connection.Open();
-                
+
                 DataTable dt = connection.GetSchema("Tables");
                 foreach (DataRow row in dt.Rows)
                 {
-                    string schemaName = (string)row[1];
+                    string schemaName = (string) row[1];
                     string tableName = (string) row[2];
-                    
-                    string query = $"COPY {schemaName}.{tableName} TO 'D:\\{tableName}.csv' WITH (FORMAT CSV, HEADER);";
-                    using (DbCommand command = connection.CreateCommand())
+
+                    string query = $"select * from {schemaName}.{tableName}";
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, (NpgsqlConnection) connection))
                     {
-                        command.CommandText = query;
-                        command.ExecuteNonQuery();
+                        using (NpgsqlDataReader dataReader = command.ExecuteReader())
+                        {
+                            for (int i = 0; i < dataReader.FieldCount; i++)
+                            {
+                                stringBuilder.Append(dataReader.GetName(i) + ",");
+                            }
+                            
+                            stringBuilder.AppendLine();
+                            
+                            while (dataReader.Read())
+                            {
+                                for (int i = 0; i < dataReader.FieldCount; i++)
+                                {
+                                    stringBuilder.Append(dataReader[i] + ",");
+                                }
+                                
+                                stringBuilder.AppendLine();
+                            }
+                        }
                     }
                 }
             }
+
+            File.WriteAllText("D:\\data.csv", stringBuilder.ToString());
         }
     }
 }
