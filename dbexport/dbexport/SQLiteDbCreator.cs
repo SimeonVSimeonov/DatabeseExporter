7818d13f55d1ed12aafc4a9dafc3939f99db30e6
ssimeonoff <simeon@rubisoft.net>
2020-02-07 12:38:34 +0200

    SQLite Db cerate implementation

---
+++ dbexport/dbexport/SQLiteDbCreator.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+
+namespace dbexport
+{
+    public class SQLiteDbCreator : IDbCreator
+    {
+        public void Create(DbConnection connection)
+        {
+            using (connection)
+            {
+                if (connection == null)
+                    return;
+
+                string[] commands =
+                {
+                    "CREATE TABLE IF NOT EXISTS Countries (Id INTEGER PRIMARY KEY AUTOINCREMENT ,Name VARCHAR(50))",
+
+                    "CREATE TABLE IF NOT EXISTS Towns(Id INTEGER PRIMARY KEY AUTOINCREMENT ,Name VARCHAR(50) ,Country_Id INT )",
+
+                    "CREATE TABLE IF NOT EXISTS Students(Id INTEGER PRIMARY KEY AUTOINCREMENT ,Name VARCHAR(30) ,Age INT, Town_Id INT )",
+                };
+
+                using (connection)
+                {
+                    connection.Open();
+
+                    foreach (var statement in commands)
+                    {
+                        ExecNonQuery(connection, statement);
+                    }
+                }
+            }
+        }
+
+        private void ExecNonQuery(DbConnection connection, string statement)
+        {
+            using (DbCommand command = connection.CreateCommand())
+            {
+                command.CommandText = statement;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
