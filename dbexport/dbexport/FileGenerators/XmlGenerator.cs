c97c632245186244e5332acbda3950a6a5caf8c9
ssimeonoff <simeon@rubisoft.net>
2020-02-18 15:19:27 +0200

    Created XmlGenerator

--- dbexport/dbexport/FileGenerators/XmlGenerator.cs
+++ dbexport/dbexport/FileGenerators/XmlGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System.Data.Common;
 using System.IO;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 using dbexport.Interfaces;
 
 namespace dbexport.FileGenerators
@@ -9,17 +11,25 @@ namespace dbexport.FileGenerators
     {
         public void Generate(IDbExtractor reader, DbConnection connection, string tableName, string path)
         {
+            //GenerateWithXmlWriter(reader, connection, tableName, path);
+            GenerateWithXmlDocument(reader, connection, tableName, path);
+        }
+
+        private void GenerateWithXmlWriter(IDbExtractor reader, DbConnection connection, string tableName, string path)
+        {
             string filePath = Path.Combine(path, $"{tableName}.xml");
+            var columns = reader.GetColumns(connection, tableName);
+            var columnNames = columns.Select(x => x.ColumnName).ToArray();
+            
             XmlWriterSettings settings = new XmlWriterSettings();
             settings.Indent = true;
             using var writer = XmlWriter.Create(filePath, settings);
-            var columns = reader.GetColumns(connection, tableName);
-
+            
             writer.WriteStartElement(tableName);
-            using var dataReader = reader.ReadData(connection, tableName, columns);
+            using var dataReader = reader.ReadData(connection, tableName, columnNames);
             while (dataReader.Read())
             {
-                writer.WriteStartElement("Entity"); //TODO can be more specific
+                writer.WriteStartElement("Entity");
                 for (int i = 0; i < dataReader.FieldCount; i++)
                 {
                     writer.WriteElementString(dataReader.GetName(i), dataReader.GetValue(i).ToString());
@@ -27,8 +37,33 @@ namespace dbexport.FileGenerators
 
                 writer.WriteEndElement();
             }
-            
+
             writer.WriteEndElement();
+        }
+
+        private void GenerateWithXmlDocument(IDbExtractor reader, DbConnection connection, string tableName,
+            string path)
+        {
+            string filePath = Path.Combine(path, $"{tableName}.xml");
+            var columns = reader.GetColumns(connection, tableName);
+            var columnNames = columns.Select(x => x.ColumnName).ToArray();
+            
+            XDocument document = new XDocument();
+            XElement root = new XElement($"{tableName}");
+            document.Add(root);
+
+            using var dataReader = reader.ReadData(connection, tableName, columnNames);
+            while (dataReader.Read())
+            {
+                XElement entity = new XElement("entity");
+                root.Add(entity);
+                for (int i = 0; i < dataReader.FieldCount; i++)
+                {
+                    entity.Add(new XAttribute(dataReader.GetName(i), dataReader.GetValue(i)));
+                }
+            }
+
+            document.Save(filePath);
         }
     }
 }
