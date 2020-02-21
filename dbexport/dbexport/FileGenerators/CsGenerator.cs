b3354badc5583e309df47c1ee5843b04b9496a0c
ssimeonoff <simeon@rubisoft.net>
2020-02-21 13:22:11 +0200

    Generate cs file with attributes.Check for nullable and string, don't need to add "?" on type of string

--- dbexport/dbexport/FileGenerators/CsGenerator.cs
+++ dbexport/dbexport/FileGenerators/CsGenerator.cs
@@ -11,7 +11,6 @@ namespace dbexport.FileGenerators
 {
     public class CsGenerator : IGenerator
     {
-        Formatter formatter = new Formatter();
         IPluralize pluralizer = new Pluralizer();
 
         public void Generate(IDbExtractor reader, DbConnection connection, string tableName, string path)
@@ -23,44 +22,59 @@ namespace dbexport.FileGenerators
             using var fileWriter = new StreamWriter(fileStream);
 
             var columns = reader.GetColumns(connection, tableName);
-          
+
+            fileWriter.WriteLine("using System.ComponentModel.DataAnnotations;");
+            fileWriter.WriteLine("using System.ComponentModel.DataAnnotations.Schema;");
+            fileWriter.WriteLine();
             fileWriter.WriteLine("namespace Entities");
             fileWriter.WriteLine("{");
-            formatter.Write(fileWriter, 1, $"public class {fileName}");
-            formatter.Write(fileWriter, 1, "{");
+            fileWriter.WriteLine(1, $"[Table(\"{tableName}\")]");
+            fileWriter.WriteLine(1, $"public class {fileName}");
+            fileWriter.WriteLine(1, "{");
             foreach (var columnInfo in columns)
             {
                 var propName = ToPascalCase(columnInfo.ColumnName);
                 var type = TypeNameOrAlias(columnInfo.ColumnType);
-                if (columnInfo.IsNullable)
+                if (columnInfo.IsPrimaryKey)
+                {
+                    fileWriter.WriteLine(2, "[Key]");
+                }
+
+                fileWriter.WriteLine(2, $"[Column(\"{propName}\")]");
+                if (columnInfo.IsNullable && columnInfo.ColumnType != typeof(string))
                 {
                     type += "?";
                 }
-               
-                formatter.Write(fileWriter, 2, "public ", type, " ", propName,
+
+                fileWriter.WriteLine(2, "public ", type, " ", propName,
                     " { get; set; }");
+                
+                var last = columns.Last();
+                if (!columnInfo.Equals(last))
+                {
+                    fileWriter.WriteLine();
+                }
             }
-            formatter.Write(fileWriter, 1, "}");
+
+            fileWriter.WriteLine(1, "}");
             fileWriter.WriteLine("}");
         }
 
         static Dictionary<Type, string> typeAlias = new Dictionary<Type, string>
         {
-            { typeof(bool), "bool" },
-            { typeof(byte), "byte" },
-            { typeof(char), "char" },
-            { typeof(decimal), "decimal" },
-            { typeof(double), "double" },
-            { typeof(float), "float" },
-            { typeof(int), "int" },
-            { typeof(long), "long" },
-            { typeof(object), "object" },
-            { typeof(sbyte), "sbyte" },
-            { typeof(short), "short" },
-            { typeof(string), "string" },
-            { typeof(uint), "uint" },
-            { typeof(ulong), "ulong" },
-            { typeof(void), "void" }
+            {typeof(bool), "bool"},
+            {typeof(byte), "byte"},
+            {typeof(char), "char"},
+            {typeof(decimal), "decimal"},
+            {typeof(double), "double"},
+            {typeof(float), "float"},
+            {typeof(int), "int"},
+            {typeof(long), "long"},
+            {typeof(sbyte), "sbyte"},
+            {typeof(short), "short"},
+            {typeof(string), "string"},
+            {typeof(uint), "uint"},
+            {typeof(ulong), "ulong"},
         };
 
         static string TypeNameOrAlias(Type type)
@@ -70,7 +84,7 @@ namespace dbexport.FileGenerators
 
             return type.Name;
         }
-        
+
         private string ToPascalCase(string text)
         {
             var words = text
